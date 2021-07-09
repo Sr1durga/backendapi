92cf39a1a74b949df33546cf47a1864df5e32d34
Sr1durga <60184610+Sr1durga@users.noreply.github.com>
2021-07-09 21:51:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const todoRoute = require("./routes/todo");
+const messageRoute = require("./routes/messages");
+const router = express.Router();
+const path = require("path");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+//middleware
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(cors());
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/todo", todoRoute);
+
+app.get('/' , (req , res) => {
+  return res.status(201).send({success : true })
+})
+
+app.listen(8090, () => {
+  console.log("Backend server is running!");
+});
